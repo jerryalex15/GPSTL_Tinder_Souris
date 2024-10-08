6cc0a0600ed4e443c73309369822c9436dc1c116
rachid <55999910+Freecs11@users.noreply.github.com>
2024-10-08 20:10:19 +0200

    backend initialisation

---
+++ backend/Alternart/src/main/java/com/gpstl/alternart/AlternartApplication.java
@@ -0,0 +1,13 @@
+package com.gpstl.alternart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlternartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlternartApplication.class, args);
+	}
+
+}
---
+++ backend/Alternart/src/test/java/com/gpstl/alternart/AlternartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpstl.alternart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlternartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
